0923fcc671e58a787565c3c9d9f9ab05a1e215bd
sanju <Impactmindztechsolutions@gmail.com>
2024-10-07 15:21:55 +0530

    update

--- src/utills/socket/Socket.js
+++ src/utills/socket/Socket.js
@@ -10,11 +10,11 @@ let userId = getLocalStorage("user")?._id;
 
 
 const socket = io(`${import.meta.env.VITE_APP_MAINURL}/`, {
-//const socket = io(`ws://localhost:3000/`, {
+//const socket = io(`http://localhost:3000/`, {
   query: {user:userId},
   reconnectionAttempts: 5, 
   transports: ['websocket'],
-  secure: true, 
+  secure: false, 
 });
 socket.on("connect", () => {
   console.log(`Connected to server `);
